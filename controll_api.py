ee614963a13ffc4e7048009cda941ba56cf0ad33
jonaw2005 <jona@wwwinkler.de>
2026-06-17 19:04:14 +0200

    test27

--- controll_api.py
+++ controll_api.py
@@ -213,5 +213,5 @@ def get_health():
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=8000, threaded=True)
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=8000)
 
